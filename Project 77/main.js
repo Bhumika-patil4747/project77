945962ca25877646f5ec84a4c0b26a948b3f1c1e
Bhumika-patil4747 <83863445+Bhumika-patil4747@users.noreply.github.com>
2021-08-01 14:18:31 +0530

    Add files via upload

---
+++ Project 77/main.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 5;
+
+var first_name = "Bhumika";
+console.log(first_name);
+
+var last_name = "Patil";
+console.log(last_name);
+
+var add = a + b ;
+console.log(add) ;
+
+var first_last_name = first_name.concat(last_name);
+console.log(first_last_name);
+
+var name_lenght_last = lenght(last_name);
+console.log(name_lenght_last);
+
+var name_lenght_first = lenght(first_name);
+console.log(name_lenght_first);
